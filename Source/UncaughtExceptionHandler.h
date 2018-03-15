b99bfc81b0435bbaf12eaeec5a6ec665ec185dce
Simon-Dox <dox2003@126.com>
2018-03-15 14:31:15 +0800

    Upload the initial code.
    Upload the initial code.

---
+++ Source/UncaughtExceptionHandler.h
@@ -0,0 +1,12 @@
+//
+//  UncaughtExceptionHandler.h
+//
+//  Created by Simon.Dong on 2018/03/15.
+//
+
+@interface UncaughtExceptionHandler : NSObject
+
++ (void)installUncaughtExceptionHandler;
+
+@end
+
